9f8639e319337affe428d6d70ec8dd523bfde731
hanhvatheanh <theanhday3112@gmail.com>
2025-01-08 20:55:53 +0700

    commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById("yes-btn").addEventListener("click", function () {
+    alert("Yêu em! ❤️");
+});
+
+document.getElementById("no-btn").addEventListener("click", function () {
+    alert("Bạn đã chọn nhầm, vui lòng chọn lại! 😢");
+});
